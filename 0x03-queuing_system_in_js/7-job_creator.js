e1d2938fe0d4209599af43833a90b41fe61768c6
mikelexx <michael.muri2020@gmail.com>
2024-09-24 18:43:25 +0300

    9. Track progress and errors with Kue: Create the Job Processor

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -58,6 +58,9 @@ const job = queue.create('push_notification_code_2', data).save((err)=>{
     console.log(`Notification job created: ${job.id}`);
   }
 });
+  job.on('failed', (err)=>{
+    console.log(`Notification job ${job.id} failed: ${err}`);
+})
   job.on('complete', ()=>console.log(`Notification job ${job.id} completed`));
   job.on('progress', (progress, _)=>{
     console.log(`Notification job ${job.id} ${progress}% complete`)
