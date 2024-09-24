acef2f46d488142e6d505f4a5293899363062487
mikelexx <michael.muri2020@gmail.com>
2024-09-24 11:00:22 +0300

    7. Create the Job processor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,5 +1,8 @@
 const kue = require('kue');
 const queue = kue.createQueue();
+queue.on('error', (err)=>{
+  console.log(`error connecting with redis server: ${err}`);
+});
 const data = {
   phoneNumber: '+2541234567',
   message: '55898234',
@@ -9,7 +12,7 @@ const job = queue.create('push_notification_code', data).save((err)=>{
     console.log('Notification job failed');
   }
   else{
-    console.log(`Notification job created: ${job.id} ID`);
+    console.log(`Notification job created: ${job.id}`);
   }
 });
 job.on('complete', ()=>console.log('Notification job completed'));
---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,12 @@
+const kue = require('kue');
+const queue = kue.createQueue();
+queue.on('error', (err)=>{
+  console.log(`error connecting with redis server: ${err}`);
+});
+function sendNotification(phoneNumber, message){
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`)
+};
+queue.process('push_notification_code', (job, done)=>{
+  sendNotification(job.data.phoneNumber, job.data.message);
+  done();
+});
