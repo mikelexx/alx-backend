c2ee29cdad6d86d7f38d9242578c8be6ecccc808
mikelexx <michael.muri2020@gmail.com>
2024-09-24 18:49:05 +0300

    handle job failed event

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,18 +1,18 @@
 const kue = require('kue');
 const queue = kue.createQueue();
-queue.on('error', (err)=>{
+queue.on('error', (err) => {
   console.log(`error connecting with redis server: ${err}`);
 });
 const data = {
   phoneNumber: '+2541234567',
-  message: '55898234',
-}
-const job = queue.create('push_notification_code', data).save((err)=>{
-  if(err){
+  message: '55898234'
+};
+const job = queue.create('push_notification_code', data).save((err) => {
+  if (err) {
     console.log('Notification job failed');
-  }
-  else{
+  } else {
     console.log(`Notification job created: ${job.id}`);
   }
 });
-job.on('complete', ()=>console.log('Notification job completed'));
+job.on('complete', () => console.log('Notification job completed'));
+job.on('failed', (_) => console.log('Notification job failed'));
